ba09dd3a9b987f249e0af4e71e3b8774e05e64b3
karishma-sayeeda <karish2906@gmail.com>
2018-10-23 17:33:00 +0530

    Update RouteConfig.cs

--- DemoAzureStorage/App_Start/RouteConfig.cs
+++ DemoAzureStorage/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DemoAzureStorage
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Item", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
